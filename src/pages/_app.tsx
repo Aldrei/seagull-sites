cc4d87821b1fe3a289106d66d8f7cc27cafa5afc
Aldrei <aldreidosantos@gmail.com>
2023-11-19 14:55:49 +0000

    :lipstick: added CSS reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components'
 import type { AppProps } from 'next/app'
 import { templateTwo } from '@/styles/themes/default'
 import { TTheme } from '@/styles/themes/default/index.d'
- 
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
