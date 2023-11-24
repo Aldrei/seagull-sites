0cc9451c6f07988082bf00cf85157692c50a82de
Aldrei <aldreidosantos@gmail.com>
2023-11-24 12:22:21 +0000

    feat(header): add header component

--- src/components/_common/Header/index.tsx
+++ src/components/_common/Header/index.tsx
@@ -1,6 +1,6 @@
-import { HeaderContainer, Title } from './styles'
+import { Bar, CloseIcon, HeaderWrapper, Logo, MenuIcon, MobileMenu, Nav, NavLink } from './styles'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 interface IProps {
   title: string;
@@ -10,18 +10,40 @@ interface IProps {
 
 
 const Header = ({ title, className }: IProps): React.ReactElement => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  
   const navigation = [
-    { name: title, href: '#', current: true },
+    { name: 'About', href: '#', current: true },
     { name: 'Team', href: '#', current: false },
     { name: 'Projects', href: '#', current: false },
     { name: 'Calendar', href: '#', current: false },
   ]
 
   return (
-    <HeaderContainer className={className}>
-      <Title>HeaderContainer INHERITED</Title>
-    </HeaderContainer>
-  )
+    <>
+      <HeaderWrapper>
+        <Logo src="/path-to-your-logo.png" alt="Logo" />
+        <Nav>
+          <NavLink href="#">Product</NavLink>
+          <NavLink href="#">Features</NavLink>
+          <NavLink href="#">Marketplace</NavLink>
+          <NavLink href="#">Company</NavLink>
+        </Nav>
+        <MenuIcon onClick={() => setMenuOpen(!isMenuOpen)}>
+          <Bar />
+          <Bar />
+          <Bar />
+        </MenuIcon>
+      </HeaderWrapper>
+      <MobileMenu isOpen={isMenuOpen}>
+        <CloseIcon onClick={() => setMenuOpen(false)}>X</CloseIcon>
+        <NavLink href="#">Product</NavLink>
+        <NavLink href="#">Features</NavLink>
+        <NavLink href="#">Marketplace</NavLink>
+        <NavLink href="#">Company</NavLink>
+      </MobileMenu>
+    </>
+  );
 }
 
 export default Header;
--- src/components/_common/Header/styles.tsx
+++ src/components/_common/Header/styles.tsx
@@ -1,9 +1,75 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export const HeaderContainer = styled.div`
-  border: 5px solid red;
-`
+// Styled components
+export const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 32px;
+  background: white;
+  border-bottom: 1px solid #eaeaea;
 
-export const Title = styled.h1`
-  color: ${(props) => props.theme.slide.banner.title.color}
-`
+  @media (max-width: 768px) {
+    // Adjust styles for mobile view
+  }
+`;
+
+export const Logo = styled.img`
+  // Add styles for your logo
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    display: none; // Hide on mobile, show dropdown instead
+  }
+`;
+
+export const NavLink = styled.a`
+  text-decoration: none;
+  color: black;
+  padding: 8px 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+export const MenuIcon = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    cursor: pointer;
+  }
+`;
+
+export const MobileMenu = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  position: absolute;
+  top: 60px; // Adjust based on actual header height
+  left: 0;
+  right: 0;
+  background: white;
+  border-top: 1px solid #eaeaea;
+  padding: 16px;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
+export const CloseIcon = styled.div`
+  // Style your close icon
+`;
+
+export const Bar = styled.div`
+  width: 20px; // Adjust width as needed
+  height: 2px; // Adjust thickness as needed
+  background-color: #007BFF; // Color of the bars
+  margin: 4px 0; // Adjust spacing between bars as needed
+`;
