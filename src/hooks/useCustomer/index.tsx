b2c5d769d6062aec2ac2286a151f4feb2cb9bdc0
Aldrei <aldreidosantos@gmail.com>
2023-11-30 22:52:04 +0000

    feat(customer): get customer data by .env

--- src/hooks/useCustomer/index.tsx
+++ src/hooks/useCustomer/index.tsx
@@ -4,7 +4,71 @@ import React, { PropsWithChildren, createContext } from 'react'
  * Hook
 */
 interface ICustomer {
-  domain: 'mydomain.com.br'
+  domain?: string
+  name?: string
+  abbr?: string
+  limitPhotosShow?: number
+  recaptcha?: {
+    api_public_key?: string,
+  }
+  social?: {
+    instagram?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    facebook?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    linkedin?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    whatsapp?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    email_1?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    email_2?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    phone_1?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+    phone_2?: {
+      desc?: string
+      link?: string
+      link_device?: string
+      title?: string
+      alt?: string
+    },
+  }
 }
 
 const useCustomer = (): ICustomer => {
@@ -16,7 +80,58 @@ const useCustomer = (): ICustomer => {
    * 
   */
   const customerDataMock: ICustomer = {
-    domain: 'mydomain.com.br'
+    domain: process.env.NEXT_PUBLIC_CUSTOMER_DOMAIN,
+    name: process.env.NEXT_PUBLIC_CUSTOMER_NAME,
+    abbr: process.env.NEXT_PUBLIC_CUSTOMER_ABBR,
+    limitPhotosShow: 4,
+    recaptcha: {
+      api_public_key: process.env.NEXT_PUBLIC_GOOGLE_RECAPTHA,
+    },
+    social: {
+      instagram: {
+        title: process.env.NEXT_PUBLIC_INSTAGRAM_TITLE,
+        desc: process.env.NEXT_PUBLIC_INSTAGRAM_DESC,
+        link: process.env.NEXT_PUBLIC_INSTAGRAM_LINK,
+        link_device: process.env.NEXT_PUBLIC_INSTAGRAM_DEVICE_LINK,
+        alt: process.env.NEXT_PUBLIC_INSTAGRAM_ALT,
+
+      },
+      facebook: {
+        title: process.env.NEXT_PUBLIC_FACEBOOK_TITLE,
+        desc: process.env.NEXT_PUBLIC_FACEBOOK_DESC,
+        link: process.env.NEXT_PUBLIC_FACEBOOK_LINK,
+        link_device: process.env.NEXT_PUBLIC_FACEBOOK_DEVICE_LINK,
+        alt: process.env.NEXT_PUBLIC_FACEBOOK_ALT,
+      },
+      linkedin: {
+        title: process.env.NEXT_PUBLIC_LINKEDIN_TITLE,
+        desc: process.env.NEXT_PUBLIC_LINKEDIN_DESC,
+        link: process.env.NEXT_PUBLIC_LINKEDIN_LINK,
+        link_device: process.env.NEXT_PUBLIC_LINKEDIN_DEVICE_LINK,
+        alt: process.env.NEXT_PUBLIC_LINKEDIN_ALT,
+      },
+      whatsapp: {
+        title: process.env.NEXT_PUBLIC_WHATSAPP_TITLE,
+        desc: process.env.NEXT_PUBLIC_WHATSAPP_DESC,
+        link: process.env.NEXT_PUBLIC_WHATSAPP_LINK,
+        link_device: process.env.NEXT_PUBLIC_WHATSAPP_DEVICE_LINK,
+        alt: process.env.NEXT_PUBLIC_WHATSAPP_ALT,
+      },
+      phone_1: {
+        title: process.env.NEXT_PUBLIC_PHONE_1_TITLE,
+        desc: process.env.NEXT_PUBLIC_PHONE_1_DESC,
+        link: process.env.NEXT_PUBLIC_PHONE_1_LINK,
+        link_device: process.env.NEXT_PUBLIC_PHONE_1_DEVICE_LINK,
+        alt: process.env.NEXT_PUBLIC_PHONE_1_ALT,
+      },
+      email_1: {
+        title: process.env.NEXT_PUBLIC_EMAIL_1_TITLE,
+        desc: process.env.NEXT_PUBLIC_EMAIL_1_DESC,
+        link: process.env.NEXT_PUBLIC_EMAIL_1_LINK,
+        link_device: process.env.NEXT_PUBLIC_EMAIL_1_DEVICE_LINK,
+        alt: process.env.NEXT_PUBLIC_EMAIL_1_ALT,
+      },
+    }
   }
 
   return customerDataMock
