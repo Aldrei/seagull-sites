a48bc4581a6f957466c116186ff072c52b39eafa
Aldrei <aldreidosantos@gmail.com>
2024-01-18 22:52:29 +0000

    feat(seo): meta tags for home and categories page

--- src/layouts/template-one/home-page/index.tsx
+++ src/layouts/template-one/home-page/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useContext } from 'react'
 
 import {
   BoxOne,
@@ -12,6 +12,7 @@ import {
 import { IMAGES } from '@/images/template-one'
 import { IHomeProps } from '@/pages/types'
 
+import { CustomerContext } from '@/hooks/useCustomer'
 import { AllPropertiesBtn, FeaturesContainer, FilterTabsContainer, ListAllPropertiesBtbContainer } from './styles'
 
 export const HomePage: React.FC<PropsWithChildren<IHomeProps>> = ({
@@ -19,13 +20,35 @@ export const HomePage: React.FC<PropsWithChildren<IHomeProps>> = ({
   filterOptionsInitial,
   propertiesFeaturedList
 }: PropsWithChildren<IHomeProps>) => {
+  const { customerData: customer } = useContext(CustomerContext)
+
   const { banners } = slideData || {}
+
+  /**
+   * TODO: Improve SEO:
+   *        
+   *       1. Implement friendly URL.
+   *       2. Implement dynamic meta tags.
+  */
+  const TITLE = `${customer.name} - ${process.env.NEXT_PUBLIC_SEO_HOME_TITLE}`.toUpperCase()
+  const KEYWORDS = process.env.NEXT_PUBLIC_SEO_HOME_KEYWORDS?.toString()?.toUpperCase()
+  const CANONICAL = customer.seo_canonical_base
+  const DESCRIPTION = `${customer.name} ${process.env.NEXT_PUBLIC_SEO_HOME_DESCRIPTION}`.toUpperCase()
+  const IMAGE = customer.logo
 
   return (
     <>
       <Head>
-        <title>My HOME page</title>
-        <meta property="og:title" content="My page title" key="title" />
+        <link rel="canonical" href={CANONICAL} />
+        <meta name="keywords" content={KEYWORDS} />
+
+        <title>{TITLE}</title>
+
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={CANONICAL} />
+        <meta property="og:image" itemProp="image"  content={IMAGE} />
       </Head>
       <Header title="TEMPLATE-ONE" logo={IMAGES.LOGO} />
       <SlideOne banners={banners} />
--- src/layouts/template-one/properties-page/index.tsx
+++ src/layouts/template-one/properties-page/index.tsx
@@ -3,24 +3,44 @@ import Head from 'next/head'
 
 import { IPropertiesPageProps } from './types'
 
+import { CustomerContext } from '@/hooks/useCustomer'
 import { IMAGES } from '@/images/template-one'
+import { useContext } from 'react'
 import { FilterContainer, PageContainer, ResultContainer } from './styles'
 
 export const PropertiesPage: React.FC<IPropertiesPageProps> = ({
   data,
   filterOptionsInitial,
 }) => {
+  const { customerData: customer } = useContext(CustomerContext)
+
   const properties = data?.data?.paginate?.data
+
+  /**
+   * TODO: Improve SEO:
+   *        
+   *       1. Implement friendly URL.
+   *       2. Implement dynamic meta tags.
+  */
+  const TITLE = `${customer.name} - ${process.env.NEXT_PUBLIC_SEO_CATEGORIES_TITLE}`.toUpperCase()
+  const KEYWORDS = process.env.NEXT_PUBLIC_SEO_CATEGORIES_KEYWORDS?.toString()?.toUpperCase()
+  const CANONICAL = `${customer.seo_canonical_base}/c`
+  const DESCRIPTION = `${customer.name} ${process.env.NEXT_PUBLIC_SEO_CATEGORIES_DESCRIPTION}`.toUpperCase()
+  const IMAGE = `${customer.logo}`
 
   return (
     <>
       <Head>
-        <title>My PROPERTIES page</title>
-        <meta
-          property="og:title"
-          content="My PROPERTIES TITLE PAGE"
-          key="title"
-        />
+        <link rel="canonical" href={CANONICAL} />
+        <meta name="keywords" content={KEYWORDS} />
+
+        <title>{TITLE}</title>
+
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={CANONICAL} />
+        <meta property="og:image" itemProp="image"  content={IMAGE} />
       </Head>
       <Header title="TEMPLATE-ONE" logo={IMAGES.LOGO} />
       <PageContainer>
