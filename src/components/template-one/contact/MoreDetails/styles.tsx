e3d5525919c25d5b4dcc59ccdb83ebc05d6c42db
Aldrei <aldreidosantos@gmail.com>
2024-01-19 23:36:50 +0000

    style(property detail): fix contact form to mobile

--- src/components/template-one/contact/MoreDetails/styles.tsx
+++ src/components/template-one/contact/MoreDetails/styles.tsx
@@ -2,6 +2,8 @@ import { styled } from 'styled-components'
 
 export const Container = styled.div`
   width: 800px;
+  max-width: 100%;
+  padding: 0 calc(${props => props.theme.spaces.sm}*2);
   margin: 50px auto 0 auto;
 
   > div {
